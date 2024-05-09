5c57a3b47b94aa32363cd3df7f9bcefb7d46b571
hxs <hxs@example.com>
2024-05-09 16:48:45 +0800

    乘客api,发送验证码服务完成

--- api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
@@ -2,6 +2,7 @@ package com.mashibing.apipassenger.controller;
 
 import com.mashibing.apipassenger.request.VerificationCodeDTO;
 import com.mashibing.apipassenger.service.VerificationCodeService;
+import com.mashibing.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,9 @@ public class VerificationCodeController {
     @Autowired
     private VerificationCodeService verificationCodeService;
     @GetMapping("/verification-code")
-    public String verificationCode(@RequestBody VerificationCodeDTO verficationCodeDTO){
+    public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verficationCodeDTO){
         String passengerPhone = verficationCodeDTO.getPassengerPhone();
-        System.out.println("接收到的手机号参数："+passengerPhone);
+        //System.out.println("接收到的手机号参数："+passengerPhone);
         return verificationCodeService.generateCode(passengerPhone);
     }
 }
--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -5,29 +5,38 @@ import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
     @Autowired
     private ServiceVerificationcodeClient serviceVerificationcodeClient;
-    public String generateCode(String passengerPhone){
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    //乘客验证码的前缀
+    private String verificationCodePrefix="passenger-verification-code";
+
+    public ResponseResult generateCode(String passengerPhone){
         //调用验证码服务，获取验证码
         System.out.println("调用验证码服务，获取验证码");
 
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationcodeClient.getNumberCode(5);
         int numberCode=numberCodeResponse.getData().getNumberCode();
-        System.out.println("remote number code:"+numberCode);
+        //System.out.println("remote number code:"+numberCode);
 
         //String code="11111";
 
-        //存入redis
-        System.out.println("存入redis");
 
+        //System.out.println("存入redis");
+        //key,value,过期时间
+        String key=verificationCodePrefix +passengerPhone;
+        //存入redis
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.SECONDS);
         //返回值
-        JSONObject result=new JSONObject();
-        result.put("code",1);
-        result.put("message","success");
-        return result.toString();
+
+        return ResponseResult.success("");
     }
 }
