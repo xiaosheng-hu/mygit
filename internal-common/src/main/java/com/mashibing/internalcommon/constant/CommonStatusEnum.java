e401a86b7ae71d87887a8d84bd794420ea94a5e7
hxs <hxs@example.com>
2024-05-09 09:36:32 +0800

    定义通用的成功和失败响应

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
@@ -0,0 +1,23 @@
+package com.mashibing.internalcommon.constant;
+
+import lombok.Getter;
+
+public enum CommonStatusEnum {
+    /**
+     * 成功
+     */
+    SUCCESS(1,"success"),
+    /**
+     * 失败
+     */
+    FAIL(0,"fail");
+    @Getter
+    private int code;
+    @Getter
+    private String value;
+
+    CommonStatusEnum(int code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+}
