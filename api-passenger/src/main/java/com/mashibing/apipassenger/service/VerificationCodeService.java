8469ed3835df0c5802e5956085a1460e6f865fe5
hxs <hxs@example.com>
2024-05-10 16:28:02 +0800

    获取redis中的验证码，并对用户输入的验证码进行校验

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,10 +1,12 @@
 package com.mashibing.apipassenger.service;
 
 import com.mashibing.apipassenger.remote.ServiceVerificationcodeClient;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
 import com.mashibing.internalcommon.response.TokenResponse;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -29,7 +31,7 @@ public class VerificationCodeService {
         //调用验证码服务，获取验证码
         System.out.println("调用验证码服务，获取验证码");
 
-        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationcodeClient.getNumberCode(5);
+        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationcodeClient.getNumberCode(6);
         int numberCode=numberCodeResponse.getData().getNumberCode();
         //System.out.println("remote number code:"+numberCode);
 
@@ -38,12 +40,21 @@ public class VerificationCodeService {
 
         //System.out.println("存入redis");
         //key,value,过期时间
-        String key=verificationCodePrefix +passengerPhone;
+        String key=generateKeyByPhone(passengerPhone);
         //存入redis
-        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.SECONDS);
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
         //返回值
 
         return ResponseResult.success("");
+    }
+
+    /**
+     * 根据手机号，生成key
+     * @param passengerPhone
+     * @return
+     */
+    public String generateKeyByPhone(String passengerPhone){
+        return verificationCodePrefix +passengerPhone;
     }
     /**
      * 校验验证码
@@ -51,9 +62,23 @@ public class VerificationCodeService {
      */
     public ResponseResult checkCode(String passengerPhone,String verificationCode){
         //根据手机号，去redis读取验证码
-        System.out.println("根据手机号，去redis读取验证码");
+        //System.out.println("根据手机号，去redis读取验证码");
+
+        //生成key
+        String key=generateKeyByPhone(passengerPhone);
+
+        //根据key，获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value："+codeRedis);
+
         //校验验证码
-        System.out.println("校验验证码");
+        if (StringUtils.isBlank(codeRedis)){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        if (!verificationCode.trim().equals(codeRedis.trim())){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        //System.out.println("校验验证码");
         //判断原来是否有用户，并进行对应的处理
         System.out.println("判断原来是否有用户，并进行对应的处理");
         //颁发令牌
