6bcd8e61405538abd0d61b57daa4e7425c0e5415
hxs <hxs@example.com>
2024-05-15 23:13:54 +0800

    读取计价规则

--- service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
@@ -1,17 +1,27 @@
 package com.mashibing.serviceprice.service;
 
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
+import com.mashibing.internalcommon.dto.PriceRule;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.response.DirectionResponse;
 import com.mashibing.internalcommon.response.ForecastPriceResponse;
+import com.mashibing.serviceprice.mapper.PriceRuleMapper;
 import com.mashibing.serviceprice.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 @Slf4j
 public class ForecastPriceService {
+    @Autowired
+    private PriceRuleMapper priceRuleMapper;
+
     @Autowired
     private ServiceMapClient serviceMapClient;
 
@@ -33,7 +43,16 @@ public class ForecastPriceService {
         log.info("距离："+distance+",时长："+duration);
 
         log.info("读取计价规则");
+        Map<String,Object> queryMap=new HashMap<>();
+        queryMap.put("city_code","110000");
+        queryMap.put("vehicle_type","1");
+        List<PriceRule> priceRules = priceRuleMapper.selectByMap(queryMap);
+        if (priceRules.size()==0){
+            return ResponseResult.fail(CommonStatusEnum.PRICE_RULE_EMPTY.getCode(),CommonStatusEnum.PRICE_RULE_EMPTY.getValue());
+        }
+        PriceRule priceRule=priceRules.get(0);
 
+        log.info("计价规则列表"+priceRules);
         log.info("根据距离、时长和计价规则，计算价格");
 
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
